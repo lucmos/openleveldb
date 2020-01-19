ed70f9bb21fd78cc0defc7de8fc3cf2f0762e479
Luca Moschella <luca.moschella94@gmail.com>
2020-01-19 22:51:31 +0100

    Reduce dependencies

--- openleveldb/__init__.py
+++ openleveldb/__init__.py
@@ -1,3 +1,3 @@
 from .database import LevelDB
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
--- tests/test_database.py
+++ tests/test_database.py
@@ -16,7 +16,7 @@ from openleveldb.database import LevelDB
 
 
 def test_version() -> None:
-    assert __version__ == "0.1.0"
+    assert __version__ == "0.1.1"
 
 
 LOCAL_TEMP_DATABASE = tempfile.mkdtemp()
