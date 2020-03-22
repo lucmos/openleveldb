69450a7895400e43e055646138f24cfe9a26cf4a
Luca Moschella <luca.moschella94@gmail.com>
2020-03-22 16:13:10 +0100

    Update dependencies

--- tests/test_database.py
+++ tests/test_database.py
@@ -17,7 +17,7 @@ from openleveldb.database import LevelDB
 
 
 def test_version() -> None:
-    assert __version__ == "0.1.3"
+    assert __version__ == "0.1.4"
 
 
 LOCAL_TEMP_DATABASE = tempfile.mkdtemp()
